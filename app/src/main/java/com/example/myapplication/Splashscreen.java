4c56a5364b10b5d82afc656507e6a1694995428f
MasBr00 <jodymardika@gmail.com>
2020-03-05 03:49:16 +0700

    SIXTH COMMIT (WITH SPLASHSCREEEEEEEEENNNNN)

---
+++ app/src/main/java/com/example/myapplication/Splashscreen.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splashscreen extends AppCompatActivity {
+
+    TextView SelamatDatang;
+    private int waktu_load = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent((getApplicationContext()), MainActivity.class));
+                finish();
+            }
+        }, waktu_load);
+    }
+}
