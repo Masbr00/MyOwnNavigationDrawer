a593836797e3d4b2baeef22ebcbacb34f5a88028
MasBr00 <jodymardika@gmail.com>
2020-03-05 16:46:50 +0700

    SEVENTH COMMIT (lil bit Improve Design)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,37 +11,37 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    DrawerLayout drawerLayout;
-    ActionBarDrawerToggle drawerToggle;
-    Toolbar toolbar;
-    NavigationView navigationView;
     boolean doubleBackToExitPressedOnce = false;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
-        toolbar.setOverflowIcon(getDrawable(R.drawable.ic_more_vert_black_24dp));
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // drawer menu setting
-        drawerLayout = findViewById(R.id.drawer);
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
-        drawerLayout.addDrawerListener(drawerToggle);
-        drawerToggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
-        drawerToggle.setDrawerIndicatorEnabled(true);
-        drawerToggle.syncState();
+        drawer = findViewById(R.id.drawer_layout);
 
-        //Select nav view first
-        navigationView = findViewById(R.id.navigation_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //fragment
+
+        /*if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view_tag,
+                    new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.home);
+        }*/
 
     }
 
@@ -70,23 +70,32 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.home:
+            case R.id.nav_home:
                 Toast.makeText(this, "HOME", Toast.LENGTH_SHORT).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view_tag,
+                        new HomeFragment()).commit();
                 break;
-            case R.id.Profile:
+            case R.id.nav_profile:
                 Toast.makeText(this, "PROFILE", Toast.LENGTH_SHORT).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view_tag,
+                        new ProfileFragment()).commit();
                 break;
         }
-        return false;
+        return true;
     }
 
     @Override
     public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
         if (doubleBackToExitPressedOnce) {
             super.onBackPressed();
             return;
         }
-
         this.doubleBackToExitPressedOnce = true;
         Toast.makeText(this, "TEKAN TOMBOL SEKALI LAGI UNTUK KELUAR", Toast.LENGTH_SHORT).show();
 
